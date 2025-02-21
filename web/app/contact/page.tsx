bfbf7f72c32f1d61bfee50f3d1e6a6de295a8ff9
yashunh <priyanshuntak@gmail.com>
2025-02-21 09:48:10 +0530

    spinner and alert

--- web/app/contact/page.tsx
+++ web/app/contact/page.tsx
@@ -4,15 +4,28 @@ import { contact } from "../actions/contact";
 import { useState } from "react";
 import ColourfulText from "@/components/ui/colourful-text";
 import Image from "next/image";
+import Spinner from "@/components/Spinner";
+import Alert from "@/components/Alert";
 
 export default function Page() {
     const [name, setName] = useState("")
     const [email, setEmail] = useState("")
     const [query, setQuery] = useState("")
+    const [loading, setLoading] = useState(false)
+    const [alert, setAlert] = useState(false)
+
+    if (loading) {
+        return <div className="flex flex-col justify-center items-center h-screen w-full">
+            <Spinner />
+        </div>
+    }
 
     return (
         <div className="flex justify-center flex-col">
-            <div className="flex justify-center mt-8 lg:mt-16">
+            <div className="m-2 lg:m-4">
+                {alert ? <Alert setAlert={setAlert} /> : <div />}
+            </div>
+            <div className="flex justify-center mt-8 lg:mt-12">
                 <div className="text-2xl md:text-5xl lg:text-7xl font-bold text-white relative z-2 font-mono w-max [filter:drop-shadow(0px_2px_4px_rgba(128,_90,_213,_0.4))]">
                     <ColourfulText text="Contact Us" />
                 </div>
@@ -25,10 +38,10 @@ export default function Page() {
                         </div>
                         <a className="rounded-full" href="https://chat.whatsapp.com/GDqMuiJDqYbIRsVm0vmX3j" title="whatsapp icons">
                             <Image src="/whatsapp.png"
-                                    alt="startup template"
-                                    width={24}
-                                    height={24}
-                                    className=""
+                                alt="startup template"
+                                width={24}
+                                height={24}
+                                className=""
                             />
                         </a>
                     </div>
@@ -54,11 +67,30 @@ export default function Page() {
                     }}
                         className="resize-none overflow-y-scroll no-scrollbar p-2 mb-3 border relative border-slate-800  bg-background rounded-lg" />
                     <div className="flex justify-center mt-2">
-                        <Button onClick={async () => {
-                            const response = await contact(name, email, query)
-                            alert("Submitted successfully, Our team will contact you soon")
-                            console.log(response)
-                        }} variant="outline">Submit</Button>
+                        <Button
+                            onClick={async () => {
+                                if (!name || !email || !query) {
+                                    window.alert("Please fill in all fields.");
+                                    return;
+                                }
+
+                            else{
+                                setLoading(true);
+                                try {
+                                    const response = await contact(name, email, query);
+                                    console.log("Response:", response);
+                                    setAlert(true);
+                                } catch (error) {
+                                    console.error("Error submitting form:", error);
+                                }
+                                setLoading(false);
+                            }
+                            }}
+                            variant="outline"
+                        >
+                            Submit
+                        </Button>
+
                     </div>
                 </div>
             </div>
